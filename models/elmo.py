2223bedd55e2df5249f00b282eee1d270b44cedc
Edi <ec9381@student.uni-lj.si>
2021-04-30 20:21:30 +0200

    Add preprocessing text for elmo

--- models/elmo.py
+++ models/elmo.py
@@ -2,26 +2,44 @@ import pandas as pd
 import keras.backend as K
 import helpers.preprocessing as pp
 from helpers.utils_elmo import *
+import re
 
-embed = hub.Module("models/ELMo/module/module_elmo")
+def replace_contraction(text):
+    contraction_patterns = [ (r'won\'t', 'will not'), (r'can\'t', 'can not'), (r'i\'m', 'i am'), (r'ain\'t', 'is not'), (r'(\w+)\'ll', '\g<1> will'), (r'(\w+)n\'t', '\g<1> not'),
+                         (r'(\w+)\'ve', '\g<1> have'), (r'(\w+)\'s', '\g<1> is'), (r'(\w+)\'re', '\g<1> are'), (r'(\w+)\'d', '\g<1> would'), (r'&', 'and'), (r'dammit', 'damn it'), (r'dont', 'do not'), (r'wont', 'will not') ]
+    patterns = [(re.compile(regex), repl) for (regex, repl) in contraction_patterns]
+    for (pattern, repl) in patterns:
+        (text, count) = re.subn(pattern, repl, text)
+    return text
+def replace_links(text, filler=' '):
+        text = re.sub(r'((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*',
+                      filler, text).strip()
+        return text
+def remove_numbers(text):
+    text = ''.join([i for i in text if not i.isdigit()])
+    return text
 
-def preprocess_text(text):
-    #text = pp.remove_stopwords(text, "")
-    text = pp.remove_mentions(text)
-    text = pp.remove_additional_spaces(text)
-    return pp.keep_chars_only(text)
+
+def cleanText(text):
+    text = text.strip().replace("\n", " ").replace("\r", " ")
+    text = replace_contraction(text)
+    text = replace_links(text, "link")
+    text = remove_numbers(text)
+    text = re.sub(r'[,!@#$%^&*)(|/><";:.?\'\\}{]',"",text)
+    text = text.lower()
+    return text
 
 model_elmo = build_model()
 model_elmo.summary()
 
 train_set = pd.read_csv("data/datasets/train.csv", encoding='utf-8')
-X = train_set['text']
+X = train_set['text'].apply(cleanText)
 y = train_set['hatespeech']
-#TODO preprocess text
+
 with tf.Session() as session:
     K.set_session(session)
     session.run(tf.global_variables_initializer())
     session.run(tf.tables_initializer())
-    history = model_elmo.fit(X, y, epochs=20, batch_size=16, validation_split = 0.2)
-    model_elmo.save_weights('models/ELMo/model_elmo_weights2.h5')
+    history = model_elmo.fit(X, y, epochs=3, batch_size=16, validation_split = 0.2)
+    model_elmo.save_weights('models/ELMo/model_elmo_weights3.h5')
 
