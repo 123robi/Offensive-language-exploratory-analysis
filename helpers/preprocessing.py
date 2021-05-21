218b89147fcb57bbc2afd7a3f8010547cfa0ab6f
Robert Košir <robikoser@gmail.com>
2021-05-21 15:56:18 +0200

    Add custom classifier to the traditional models

--- helpers/preprocessing.py
+++ helpers/preprocessing.py
@@ -41,3 +41,18 @@ def keep_chars_only(data):
     regex = re.compile("[^a-zA-Z]")
     return data.str.replace(regex, " ")
 
+
+def single_string_remove_mention(data):
+    regex = re.compile(r"/@[A-Za-z0-9_-]*/g")
+    return regex.sub("", data)
+
+
+def single_keep_chars_only(data):
+    regex = re.compile("[^a-zA-Z]")
+    return regex.sub(" ", data)
+
+
+def single_remove_spaces(data):
+    regex = re.compile("r\s+")
+    return regex.sub(" ", data)
+
